8ac063b3a574b8eeb3e7902f4c84c6595e6bd4f9
yamato-0711 <55276549+yamato-0711@users.noreply.github.com>
2019-11-16 16:58:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -103,6 +103,62 @@ $(function () {
         })
 })
 
+//pagetopのやつ
+$(document).ready(function () {
+
+    var pagetop = $('.pagetop');
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() > 100) {
+
+            pagetop.fadeIn();
+
+        } else {
+
+            pagetop.fadeOut();
+
+        }
+
+    });
+
+    pagetop.click(function () {
+
+        $('body, html').animate({ scrollTop: 0 }, 500);
+
+        return false;
+    });
+
+});
+
+$(document).ready(function () {
+
+    var pagetop = $('.footer-content');
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() > 100) {
+
+            pagetop.fadeIn();
+
+        } else {
+
+            pagetop.fadeOut();
+
+        }
+
+    });
+
+    pagetop.click(function () {
+
+        $('body, html').animate({ scrollTop: 0 }, 500);
+
+        return false;
+    });
+
+});
+
+
 
 
 //fadein用のfunction
